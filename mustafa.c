2b0af3a3570803354bce76752b033adffdaae671
Mustafa12388 <elfikymustafa88@gmail.com>
2022-12-20 22:31:24 +0200

    data

---
+++ mustafa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (void)
+{
+   int a,b,c;
+   printf("please enter your data in sequence : \n ");
+   scanf("%d %d",&a,&b);
+   printf("your summation is %d",a+b);
+   return 0;
+
+
+
+}
